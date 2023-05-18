5b468888f514c78f0308785bdc3e24781f562f05
codergalib20 <coderboygalif@gmail.com>
2023-05-18 21:11:23 +0600

    added pages/_app.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+
+interface Props {};
+
+const Loading :FC<Props> = () => {
+    return (
+         <div>
+             Loading
+         </div>
+    )
+};
+
+export default Loading
