e11d9f570e00168cbdf7a49dc19143b3826a95cd
codergalib20 <coderboygalif@gmail.com>
2023-05-19 02:40:56 +0600

    start customizing course pages sidebar

---
+++ src/components/leftSide/sections.tsx
@@ -0,0 +1,35 @@
+const section: string[] = [
+    "Routing",
+    "Rendering",
+    "Data Fetching",
+    "Styling",
+    "Optimizing",
+    "Configuring",
+    "Deploying",
+    "Upgrading",
+    "Upgrading",
+    "Upgrading",
+    "API Reference",
+    "API Reference",
+    "API Reference",
+    "Components",
+    "Components",
+    "Components",
+    "File Convention",
+    "File Convention",
+    "File Convention",
+    "Functions",
+    "Functions",
+    "Functions",
+    "next.config.js",
+    "next.config.js",
+    "next.config.js",
+]
+
+export default function Sections() {
+    return (
+        <div>
+
+        </div>
+    )
+}
