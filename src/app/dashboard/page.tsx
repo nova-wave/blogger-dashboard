672bb0b204d242bd18bb9840f66bb298d5624b79
codergalib20 <coderboygalif@gmail.com>
2023-05-18 23:10:44 +0600

    layout exploring

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,6 @@ import RootLayout from "../layout";
 
 export default function Page() {
     return (
-        <RootLayout>
-            <h1>Hello dashboard</h1>
-        </RootLayout>
+        <h1>Hello dashboard</h1>
     )
 }
