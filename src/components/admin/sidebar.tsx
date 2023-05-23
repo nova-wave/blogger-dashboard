ee215ca3b91b1a164e61da312e2ee392f1235166
codergalib20 <coderboygalif@gmail.com>
2023-05-24 02:08:49 +0600

    generate admin page sidebar local data with icons and name with typescript types

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+import { sidebar } from "./localData/sidebar";
+
 export default function Sidebar() {
     return (
         <div className="p-3">
-            
+            {sidebar.map((item, index) => {
+                return (
+                    <Link href={`/`} key={index}>
+                        <div>{item.name}</div>
+                    </Link>
+                )
+            })}
         </div>
     )
 }
