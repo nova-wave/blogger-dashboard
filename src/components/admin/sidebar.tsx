9c83c5c770c3cc0ef3ecf78a33946d519509771d
codergalib20 <coderboygalif@gmail.com>
2023-05-24 02:28:02 +0600

    commited

--- src/components/admin/sidebar.tsx
+++ src/components/admin/sidebar.tsx
@@ -1,13 +1,17 @@
+"use client"
+
 import Link from "next/link";
 import { sidebar } from "./localData/sidebar";
-
+import { useSearchParams } from "next/navigation";
 export default function Sidebar() {
+    const searchParams = useSearchParams()
+    const tab: string | undefined = (searchParams as any).get('tab')
     return (
         <div className="p-3">
             {sidebar.map((item, index) => {
                 return (
                     <Link legacyBehavior href={`/admin?tab=${item.link}`} key={index}>
-                        <a className={`flex items-center gap-6 text-lg font-light mt-7 text-yellow-300`}>
+                        <a className={`${tab === item.link ? "text-yellow-300" : "text-white"} ${(!tab && item.link === 'admin') && "text-yellow-300"} flex items-center gap-6 text-lg font-light mt-7`}>
                             <item.icon size={30} />
                             <div>{item.name}</div>
                         </a>
