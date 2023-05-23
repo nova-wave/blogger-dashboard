b2db001adc89064a02ac08e25b9f32467fba9711
codergalib20 <coderboygalif@gmail.com>
2023-05-24 02:05:38 +0600

    generate admin page sidebar local data with icons and name

--- src/components/admin/localData/sidebar.ts
+++ src/components/admin/localData/sidebar.ts
@@ -1,3 +1,41 @@
-export  const sidebar = [
-    {}
-]
+import {
+  AiOutlineHome,
+  AiOutlineBarChart,
+  AiOutlineMail,
+  AiFillSetting,
+} from "react-icons/ai";
+import { SiReacthookform } from "react-icons/si";
+import { CgProfile } from "react-icons/cg";
+
+export const sidebar = [
+  {
+    icon: AiOutlineHome,
+    name: "Admin",
+    link: "/admin",
+  },
+  {
+    icon: AiOutlineBarChart,
+    name: "Chart",
+    link: "/chart",
+  },
+  {
+    icon: SiReacthookform,
+    name: "",
+    link: "",
+  },
+  {
+    icon: AiOutlineMail,
+    name: "",
+    link: "",
+  },
+  {
+    icon: CgProfile,
+    name: "",
+    link: "",
+  },
+  {
+    icon: AiFillSetting,
+    name: "",
+    link: "",
+  },
+];
