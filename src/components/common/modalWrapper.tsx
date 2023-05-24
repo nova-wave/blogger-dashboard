36a098abfb38d0ab64fdb8f8f14fdc6da0240f14
codergalib20 <coderboygalif@gmail.com>
2023-05-24 20:13:56 +0600

    added custom modal wrapper styles

--- src/components/common/modalWrapper.tsx
+++ src/components/common/modalWrapper.tsx
@@ -10,9 +10,9 @@ export default function ModalWrapper({ children, title }: PropsTypes) {
     <div className="fixed left-2/4 top-2/4 transform -translate-x-2/4 -translate-y-2/4 bg-dark border-2 border-[#333] p-2 shadow-lg shadow-teal-950 rounded min-w-[300px]">
       {/* modal header */}
       <div className="flex items-center justify-between">
-        <div>{title}</div>
+        <h2 className="text-red-600 font-medium text-lg">{title}</h2>
         <div className="flex justify-end items-center">
-          <button className="text-white p-2 border border-red-700">
+          <button className="text-white p-2 border-2 bg-red-700 rounded-full hover:bg-red-600">
             <MdOutlineClose color="white" size={22} />
           </button>
         </div>
