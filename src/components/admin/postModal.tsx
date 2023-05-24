948a841688f4ae9cb5e1f1cfd0cae662f6a28eb4
codergalib20 <coderboygalif@gmail.com>
2023-05-25 00:27:39 +0600

    creating markdown preview html customize

--- src/components/admin/postModal.tsx
+++ src/components/admin/postModal.tsx
@@ -1,6 +1,3 @@
-import ReactMarkdown from "react-markdown";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
 import MarkdownTextarea from "./markdownTextarea";
 import { useState } from "react";
 import PreviewMarkdown from "./previewMarkdown";
--- src/components/admin/previewMarkdown.tsx
+++ src/components/admin/previewMarkdown.tsx
@@ -1,9 +1,40 @@
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 interface PropsTypes {
   markdownContent: string;
 }
 
 const PreviewMarkdown: React.FC<PropsTypes> = ({ markdownContent }) => {
-  return <div>Preview Markdown</div>;
+  return (
+    <div className="markdown-preview-custom">
+      <ReactMarkdown
+        components={{
+          code: ({ node, inline, className, children, ...props }) => {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={solarizedlight as any}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+                // style={{} as CSSProperties} // Explicitly type the style prop
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdownContent}
+      </ReactMarkdown>
+    </div>
+  );
 };
 
 export default PreviewMarkdown;
