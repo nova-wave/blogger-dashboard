1c4f8b84b0b123a50e7ea63f71ab3f25ca048348
codergalib20 <coderboygalif@gmail.com>
2023-05-24 20:03:16 +0600

    added custom modal wrapper styles

--- src/components/common/modalWrapper.tsx
+++ src/components/common/modalWrapper.tsx
@@ -1,10 +1,17 @@
-export default function ModalWrapper({
-  children,
-}: {
+interface PropsTypes {
   children: React.ReactNode;
-}) {
+  title?: string;
+}
+
+export default function ModalWrapper({ children, title }: PropsTypes) {
   return (
-    <div className="fixed left-2/4 top-2/4 transform -translate-x-2/4 -translate-y-2/4 bg-dark">
+    <div className="fixed left-2/4 top-2/4 transform -translate-x-2/4 -translate-y-2/4 bg-dark border-2 border-[#333] p-2 shadow-lg shadow-teal-950 rounded">
+      {/* modal header */}
+      <div>
+        <div>{title}</div>
+        <div></div>
+      </div>
+      {/* modal content */}
       {children}
     </div>
   );
