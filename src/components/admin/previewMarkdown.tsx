ae0027334e4e6be2fa15efd8ca87457dc85ea142
codergalib20 <coderboygalif@gmail.com>
2023-05-26 15:56:03 +0600

    install npm remarGFm

--- src/components/admin/previewMarkdown.tsx
+++ src/components/admin/previewMarkdown.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkGfm from "remark-gfm";
 
 interface PropsTypes {
   markdownContent: string;
@@ -10,6 +11,7 @@ const PreviewMarkdown: React.FC<PropsTypes> = ({ markdownContent }) => {
   return (
     <div className="markdown-preview-custom">
       <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
         components={{
           code: ({ node, inline, className, children, ...props }) => {
             const match = /language-(\w+)/.exec(className || "");
