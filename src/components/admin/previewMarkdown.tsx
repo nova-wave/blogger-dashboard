a7ee1fd830139abcf07d8b6437b8859fe5c73f8e
codergalib20 <coderboygalif@gmail.com>
2023-05-26 20:47:49 +0600

    creating new doc modal

--- src/components/admin/previewMarkdown.tsx
+++ src/components/admin/previewMarkdown.tsx
@@ -5,7 +5,7 @@ import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import React from "react";
-// import rehypeRaw from "rehype-raw";
+import rehypeRaw from "rehype-raw";
 
 interface PropsTypes {
   markdownContent: string;
@@ -16,8 +16,7 @@ const PreviewMarkdown: React.FC<PropsTypes> = ({ markdownContent }) => {
     <div className="markdown-preview-custom">
       <ReactMarkdown
         // remarkPlugins={[[remarkMath, remarkGfm, { singleTilde: false }]]}
-        // rehypePlugins={[rehypeKatex]}
-        // remarkPlugins={[rehypeRaw]}
+        rehypePlugins={[rehypeRaw]}
         components={{
           code: ({ node, inline, className, children, ...props }) => {
             const match = /language-(\w+)/.exec(className || "");
@@ -39,7 +38,7 @@ const PreviewMarkdown: React.FC<PropsTypes> = ({ markdownContent }) => {
           },
         }}
       >
-        {(markdownContent as any).match(/<details>([\s\S]*?)<\/details>/g)}
+        {markdownContent}
       </ReactMarkdown>
     </div>
   );
