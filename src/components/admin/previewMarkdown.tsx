04b32f0ec13b14a7dc9be79bb545403aa2c8c509
codergalib20 <coderboygalif@gmail.com>
2023-05-26 16:44:54 +0600

    install npm remarGFm

--- src/components/admin/previewMarkdown.tsx
+++ src/components/admin/previewMarkdown.tsx
@@ -2,16 +2,23 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import React from 'react';
 
 interface PropsTypes {
   markdownContent: string;
 }
 
 const PreviewMarkdown: React.FC<PropsTypes> = ({ markdownContent }) => {
+  const renderers = {
+    listItem: ({ children }: { children: any }) => <div>{children}</div>,
+  };
   return (
     <div className="markdown-preview-custom">
       <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
+        // remarkPlugins={[[remarkMath, remarkGfm, { singleTilde: false }]]}
+        // rehypePlugins={[rehypeKatex]}
         components={{
           code: ({ node, inline, className, children, ...props }) => {
             const match = /language-(\w+)/.exec(className || "");
@@ -30,6 +37,22 @@ const PreviewMarkdown: React.FC<PropsTypes> = ({ markdownContent }) => {
                 {children}
               </code>
             );
+          },
+          li: ({ children }) => (
+            <li>
+              ==== {children}
+            </li>
+          ),
+          p: ({ node, children }) => {
+            const isSummary = React.Children.toArray(children).some(
+              (child) => (child as any).type === "summary"
+            );
+
+            if (isSummary) {
+              return <details>{children}</details>;
+            }
+
+            return <p>{children}</p>;
           },
         }}
       >
