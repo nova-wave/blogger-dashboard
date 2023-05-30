2d9d488ff0ac2b672cee6c358b52e258e0ff837f
codergalib20 <coderboygalif@gmail.com>
2023-05-30 20:15:24 +0600

    ading new feature of section

--- src/components/admin/sections.tsx
+++ src/components/admin/sections.tsx
@@ -1,9 +1,31 @@
-const Sections = () =>{
-    return (
-        <div>
-            Sections
-        </div>
-    )
+import { useState } from "react";
+
+const languagesData = ["Python", "Javascript", "Go", "C++", "C#", "C"];
+
+const Sections = () => {
+  const [languages, setLanguages] = useState("");
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <select name="topic" id="">
+            {languagesData.map((lang, index) => (
+              <option value={lang} key={index}>
+                {lang}
+              </option>
+            ))}
+          </select>
+          <input
+            type="number"
+            name="order"
+            placeholder="select order number"
+            min={0}
+            max={50}
+          />
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Sections;
