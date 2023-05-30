9cff0e24136f68f78f7b7797a08a9110e24d9a97
codergalib20 <coderboygalif@gmail.com>
2023-05-30 20:09:53 +0600

    ading new feature of section

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import PostPage from "@/components/admin/postPage";
+import Sections from "@/components/admin/sections";
 import { useSearchParams } from "next/navigation";
 
 function Page() {
   const searchParams = useSearchParams();
   const tab: string | undefined = (searchParams as any).get("tab");
 
-  return <div className="p-4 pl-0">{tab === "new-doc" && <PostPage />}</div>;
+  return (
+    <div className="p-4 pl-0">
+      {tab === "new-doc" && <PostPage />}
+      {tab === "sections" && <Sections />}
+    </div>
+  );
 }
 
 export default Page;
