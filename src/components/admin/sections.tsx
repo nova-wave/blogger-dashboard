95ce1fbcbb28875d505c2086dfded9c3d2464671
codergalib20 <coderboygalif@gmail.com>
2023-05-30 20:56:44 +0600

    ading new feature of section

--- src/components/admin/sections.tsx
+++ src/components/admin/sections.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-
+import { AiOutlineAppstoreAdd } from "react-icons/ai";
 const languagesData = ["Python", "Javascript", "Go", "C++", "C#", "C"];
 
 export default function SectionsPage() {
@@ -13,9 +13,9 @@ export default function SectionsPage() {
     });
   };
   return (
-    <div>
-      <form>
-        <fieldset>
+    <div className="grid grid-cols-12 gap-3">
+      <form className="col-span-8 grid grid-cols-6 gap-2">
+        <fieldset className="col-span-5">
           <div className="grid grid-cols-2 gap-4">
             <select
               onChange={handleFieldsChange}
@@ -29,6 +29,7 @@ export default function SectionsPage() {
               ))}
             </select>
             <input
+              required
               onChange={handleFieldsChange}
               className="bg-dark py-2 px-3 outline-none border-2 border-emerald-700 text-emerald-600 text-md placeholder:text-emerald-800"
               type="number"
@@ -39,13 +40,23 @@ export default function SectionsPage() {
             />
           </div>
           <input
+            required
             onChange={handleFieldsChange}
             className="bg-dark py-2 px-3 outline-none border-2 border-emerald-700 text-emerald-600 w-full text-md placeholder:text-emerald-800"
             type="text"
             placeholder="Write section name"
           />
         </fieldset>
+        <div className="col-span-1">
+          <button
+            type="submit"
+            className="py-4 px-4 bg-emerald-700 text-gray-200 rounded border-2 border-gray-200 hover:text-emerald-700 hover:border-emerald-700 hover:bg-gray-200"
+          >
+            <AiOutlineAppstoreAdd />
+          </button>
+        </div>
       </form>
+      <div className="col-span-4"></div>
     </div>
   );
 }
