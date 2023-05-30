ba0684091bf9d88174819db267ce57f2360ca76e
codergalib20 <coderboygalif@gmail.com>
2023-05-30 21:27:47 +0600

    ading new feature of section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import axios from "axios";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
+axios.create({
+  baseURL: process.env.NEXT_PUBLIC_PRODUCTION_URL,
+});
 
 export const metadata = {
-  title: 'Blogger',
-  description: 'blogger application made for documentory',
-}
+  title: "Blogger",
+  description: "blogger application made for documentory",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#000000] text-[#eaeaea]`}>
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </body>
-    </html >
-  )
+    </html>
+  );
 }
